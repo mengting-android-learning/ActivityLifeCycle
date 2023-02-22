42c7d5282020e11fe73bdc6be3029026af07c301
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-22 09:59:02 +0800

    [feat]:add log info

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -2,17 +2,44 @@ package com.example.activitylifecycle
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "onCreate")
         setContentView(R.layout.activity_main)
         val button1: Button = findViewById(R.id.button1)
         button1.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
     }
 }
--- app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
@@ -1,11 +1,38 @@
 package com.example.activitylifecycle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
+        Log.d("SecondActivity", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SecondActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SecondActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SecondActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SecondActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SecondActivity", "onDestroy")
     }
 }
