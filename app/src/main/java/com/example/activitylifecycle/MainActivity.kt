e125e1f3820a26697efd178ab7b0fb4721778e8e
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-22 09:44:43 +0800

    [feat]:add two activities and Intent

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.activitylifecycle
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
